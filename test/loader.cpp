e3b2df4ea0ddae4b0c303b8371792c37e42d7623
Yassine MADDOURI <m.y.maddouri@gmail.com>
2018-02-18 18:27:37 +0100

    test/loader Fix typo and add usage comment

--- test/loader.cpp
+++ test/loader.cpp
@@ -3,9 +3,10 @@
 #include <cstdint>
 #include <sstream>
 
+// usage: loader "path/to/shared/lib/dir"
 int main(int argc, char* argv[])
 {
-    dynalo::library lib(std::string(argv[1]) + "/" + dynalo::to_native_name("awesome"));
+    dynalo::library lib(std::string(argv[1]) + "/" + dynalo::to_native_name("shared"));
 
     auto add_integers  = lib.get_function<int32_t(const int32_t, const int32_t)>("add_integers");
     auto print_message = lib.get_function<void(const char*)>("print_message");
