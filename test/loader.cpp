6c59fa08936b99ca9490b227787f575bec9be862
s.kolesnik <7fosGbc9S>
2021-05-26 16:49:18 +0500

    added try-catch block for loader. Added check for input.

--- test/loader.cpp
+++ test/loader.cpp
@@ -1,17 +1,33 @@
 #include <dynalo/dynalo.hpp>
 
 #include <cstdint>
+#include <iostream>
 #include <sstream>
 
 // usage: loader "path/to/shared/lib/dir"
-int main(int /*argc*/, char* argv[])
+int main(int argc, char* argv[])
 {
-    dynalo::library lib(std::string(argv[1]) + "/" + dynalo::to_native_name("shared"));
+    if (argc < 2)
+    {
+        std::cerr << "No path for a dynamic library was provided.\nUsage: ./loader path_to_dyn_lib" << std::endl;
+    }
+    try
+    {
+        dynalo::library lib(std::string(argv[1]) + "/" + dynalo::to_native_name("shared"));
 
-    auto add_integers  = lib.get_function<int32_t(const int32_t, const int32_t)>("add_integers");
-    auto print_message = lib.get_function<void(const char*)>("print_message");
+        auto add_integers  = lib.get_function<int32_t(const int32_t, const int32_t)>("add_integers");
+        auto print_message = lib.get_function<void(const char*)>("print_message");
 
-    std::ostringstream oss;
-    oss << "it works: " << add_integers(1, 2);
-    print_message(oss.str().c_str());
+        std::ostringstream oss;
+        oss << "it works: " << add_integers(1, 2);
+        print_message(oss.str().c_str());
+
+        return 0;
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "Test failed: " << error.what() << std::endl;
+    }
+
+    return -1;
 }
